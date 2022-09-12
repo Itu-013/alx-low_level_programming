eaa39d7943ea078552c2588fe13e55d2d799e5e7
Adenike <itunuade287@gmail.com>
2022-09-12 16:31:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 /**
  * main - the function that houses the command
- *
  * this program is to print a statement
  *
  * Return: will be 1
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
