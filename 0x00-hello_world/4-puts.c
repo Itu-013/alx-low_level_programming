9abaf81fd0f48383c35b765227d0cbf600c4ff12
Adenike <itunuade287@gmail.com>
2022-09-12 13:16:12 -0700

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* the code below, embedded in the main function, is basically
+* to print a text using the puts function and return 0
+* this is a comment
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
