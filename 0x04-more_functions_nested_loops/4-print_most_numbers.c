a09d80844faa92786b70761e4daf30c977b9598a
Adenike <itunuade287@gmail.com>
2022-10-18 07:15:41 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		if (n != 2 &&  n != 4)
+		if ((n != 2) &&  (n != 4))
 		{
-			_putchar(i);
+			_putchar('0' + n);
 		}
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-git
+print num
